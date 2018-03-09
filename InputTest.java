cf97df37285cc822fd099361ccdb645bef0e3289
yuyidielan <2267476472@qq.com>
2018-03-10 00:27:20 +0800

    Delete InputTest.java

--- InputTest.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class InputTest{
-     public static void main(String[] args){
-       Scanner input=new Scanner(System.in);
-       System.out.println(input.next());
-      
-       input.close();
-     }
-}
