136ed73b6a202f77ae5f6c40124ba462f2fc8fa4
yuyidielan <2267476472@qq.com>
2018-03-10 08:17:38 +0800

    Add files via upload

---
+++ Chapter1/IfTest2.java
@@ -0,0 +1,12 @@
+class IfTest2{
+	public static void main(String[] args){
+	int x=2;
+	if(x==3){
+	System.out.println("x must be 3");
+	}else{
+	System.out.println("x is NOT 3");
+	}
+	System.out.println("Thisruns no matter what");
+	}
+
+}
