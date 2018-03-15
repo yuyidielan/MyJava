03917057b8d1bcc930bd97359c16188f4163fa0a
yuyidielan <2267476472@qq.com>
2018-03-15 23:43:27 +0800

    Add files via upload

---
+++ Chapter2/TapeDeckTestDrive.java
@@ -0,0 +1,19 @@
+class TapeDeck{
+	boolean canRecord=false;
+	void playTape(){
+		System.out.println("tape playing");
+	}
+	void recordTape(){
+		System.out.println("tape recoding ");
+	}
+}
+class TapeDeckTestDrive{
+	public static void main(String[] args){
+	TapeDeck t=new TapeDeck();
+	t.canRecord=true;
+	t.playTape();
+	if(t.canRecord==true){
+		t.recordTape();
+	}
+	}
+}
